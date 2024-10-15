793f9fd93429cf4000ff0b182500d3c483678209
Ivanov Ivan Ivanovich <Ivanov@example.com>
2024-10-15 23:21:39 +0300

    2 commit

--- test/test_CalcRating.py
+++ test/test_CalcRating.py
@@ -5,7 +5,7 @@ import pytest
 RatingsType = dict[str, float]
 
 
-class TestCalcRating:
+clas TestCalcRating:
     @pytest.fixture()
     def input_data(self) -> tuple[DataType, RatingsType]:
         data: DataType = {
